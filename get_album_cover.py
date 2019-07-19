e5f84490bfc9ae4e85e08c554b8751c02ccee80c
Sean Turner <Sean-Turner@idexx.com>
2019-07-19 10:20:01 -0400

    first pass at argparse. Thinking of switching to a class

--- get_album_cover.py
+++ get_album_cover.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import requests
 import shutil
@@ -8,12 +9,11 @@ import time
 from spotipy import util, client
 
 
-def get_album_cover(*, currently_playing, current_album_uri, save_path):
+def get_album_cover(*, currently_playing, current_album_uri, save_path, verbosity_count):
     """Return the current album uri.
 
     TODO: More docstrings here
     """
-
     # If nothing is playing, return none
     if currently_playing is None:
         return
@@ -38,14 +38,17 @@ def get_album_cover(*, currently_playing, current_album_uri, save_path):
     return current_album_uri
 
 
-def main():
+def main(*, save_path, update_seconds, verbosity):
+    """Get album cover."""
     username = os.environ['SPOTIFY_USERNAME']
     client_id = os.environ['SPOTIFY_CLIENT_ID']
     client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
-    save_path = 'output.png'
-    update_seconds = 5
 
     current_album_uri = None
+
+    if None in (username, client_id, client_secret):
+        raise(CoverGetterException, "Could not get environment variables.")
+        return -1
 
     while True:
 
@@ -63,9 +66,29 @@ def main():
             currently_playing = sp_client.currently_playing()
             current_album_uri = get_album_cover(currently_playing=currently_playing,
                                                 current_album_uri=current_album_uri,
-                                                save_path=save_path)
-            time.sleep(update_seconds)
+                                                save_path=save_path,
+                                                verbosity=verbosity)
+
+        time.sleep(update_seconds)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Grab your currently playing album cover.')
+    parser.add_argument('-p', '--path', help='Location to save album art', default='output.png')
+    parser.add_argument('-i', '--interval',
+                        help='Time to wait between checks',
+                        type=float,
+                        default=5)
+    parser.add_argument('-v', '--verbosity', action='store_true', default=0)
+
+    args = parser.parse_args()
+
+    main(save_path=args.path,
+         update_seconds=args.interval,
+         verbosity=args.verbosity)
+
+
+class CoverGetterException(Exception):
+    """Exception class for CoverGetter."""
+
+    pass
